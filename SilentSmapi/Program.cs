b2583c7991ab50bb64e8491a5ffdc71f88fe8b08
Tim (Radical Idiot) <76754136+Denveous@users.noreply.github.com>
2024-10-19 16:50:44 -0400

    Add project files.

---
+++ SilentSmapi/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+string[] programArguments = Environment.GetCommandLineArgs();
+string[] smapiArguments = programArguments.Skip(1).ToArray();
+using (var process = new Process())
+{
+    process.StartInfo.FileName = "StardewModdingAPI.exe";
+    process.StartInfo.Arguments = string.Join(" ", smapiArguments); // Combine arguments into a single string
+    process.StartInfo.UseShellExecute = false;
+    process.StartInfo.CreateNoWindow = true;
+    process.Start();
+}
+Console.WriteLine("Running Smapi in Silent mode...");
+
