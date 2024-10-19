1398d6f5e53b640935c224d110ff13d68a1a22e5
Tim (Radical Idiot) <76754136+Denveous@users.noreply.github.com>
2024-10-19 19:14:43 -0400

    Fixing old namespace name

--- SilentSmapi/Program.cs
+++ SilentSmapi/Program.cs
@@ -2,13 +2,8 @@
 namespace SilentSmapi {
     class Program {
         static string[] programArguments = Environment.GetCommandLineArgs();
-        static string[] smapiArguments = programArguments.Skip(1)
-            .Concat(new[] { "--developer-mode-off", "--no-terminal" })
-            .ToArray();
-        public static bool IsSMAPIRunning() {
-            return Process.GetProcesses()
-                .Any(p => p.MainWindowTitle.Contains("running SMAPI", StringComparison.OrdinalIgnoreCase));
-        }
+        static string[] smapiArguments = programArguments.Skip(1).Concat(new[] { "--developer-mode-off", "--no-terminal" }).ToArray();
+        public static bool IsSMAPIRunning() { return Process.GetProcesses().Any(p => p.MainWindowTitle.Contains("running SMAPI", StringComparison.OrdinalIgnoreCase)); } 
         static void Main(string[] args) {
             Console.Title = "SilentSmapi";
             using (var process = new Process()) {
@@ -21,17 +16,15 @@ namespace SilentSmapi {
                     Console.Write("Starting SMAPI in \"Silent\" mode...\n");
                     while (true) {
                         Thread.Sleep(100);
-                            if (IsSMAPIRunning() == true) {
-                                Thread.Sleep(1000);
-                                Console.WriteLine("StardewModdingAPI.exe has finished loading! Hiding...");
-                                Thread.Sleep(2500);
-                                Environment.Exit(0); 
-                            }
+                        if (IsSMAPIRunning() == true) {
+                            Thread.Sleep(1000);
+                            Console.WriteLine("StardewModdingAPI.exe has finished loading! Hiding...");
+                            Thread.Sleep(2500);
+                            Environment.Exit(0); 
+                        }
                     }
                 }
-                catch (Exception ex) {
-                    Console.WriteLine("Error starting or monitoring SMAPI process: " + ex.Message + " (make sure this is in the root Stardew directory)");
-                }
+                catch (Exception ex) { Console.WriteLine("Error starting or monitoring SMAPI process: " + ex.Message + " (make sure this is in the root Stardew directory)"); }
             }
         }
     }
